b16b3d7810d8f1fb851ed737d0c8c76011e6adc5
Michael Shipper <mshipper@yahoo.com>
2025-09-06 16:55:28 -0500

    work in pogress

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { default } from './LinkedBundleNodeMap';
+import FormContext,{StatusContextInterface,OnChange,CoreEvent,OnClick,LinkedMapStatus} from './FormContext';
+export {OnChange,CoreEvent,OnClick,LinkedMapStatus,FormContext} 
+
