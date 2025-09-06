10905a56eb2199e8f0c8744901347b04775290c7
Michael Shipper <mshipper@yahoo.com>
2025-09-06 17:04:00 -0500

    work in pogress

---
+++ test_page/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve static files from the 'build' directory (or 'dist')
+app.use(express.static(path.join(__dirname, 'public'))); // Adjust 'build' as per your SPA framework
+
+// For any other route, serve the main index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Adjust 'build' as per your SPA framework
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
