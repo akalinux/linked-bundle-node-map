a86a8ee8163a9e844b3cf45750d49834d98f31bf
Michael Shipper <mshipper@yahoo.com>
2025-11-01 14:27:16 -0500

    Work in progress

--- demo_app/src/App.tsx
+++ demo_app/src/App.tsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import 'linked-bundle-node-map/dist/linked-bundle-node.css'
 import MinimalDemo from './demos/MinimalDemo/MinimalDemo'
+
+const maps: {[key:string]: ()=>React.ReactElement}={
+	min: MinimalDemo,
+};
 const el = document.getElementById('app');
 if (el !== null) {
 	const root = ReactDOM.createRoot(el);
+	const key=window.location.href.replace(/^.*\?demo=/,'');
+	console.log(key)
+	const Demo=maps.hasOwnProperty(key) ? maps[key] : MinimalDemo;
 
 	root.render(<div style={{ width: '100%', height: '100%' }}>
-		<MinimalDemo />
+		<Demo />
 	</div>)
 }
